71ccaf7182ebfd6792f14b33722540da7197043b
JeffafaV <47927752+JeffafaV@users.noreply.github.com>
2022-05-02 18:29:47 -0700

    Update dish.cpp
    Added delete functions for the current ingredient list and ingredient database. Also added a menu in the main function.

--- dish.cpp
+++ dish.cpp
@@ -9,12 +9,15 @@ class Ingredient
 	public:
 	Ingredient();
     
+	void display(vector<string> v);
     void set_cur_names();
 	void set_db_names();
 	void set_db_index();
 	
 	void add_cur_names(string new_name);
 	void add_db_names_index(string new_name);
+	void del_cur_names(string del_name);
+	void del_db_names_index(string del_name);
     
     vector<string> get_cur_names();
 	vector<string> get_db_names();
@@ -31,6 +34,14 @@ Ingredient::Ingredient()
     vector<string> cur_names = {};
 	vector<string> db_names = {};
 	vector<int> db_index = {};
+}
+
+void Ingredient::display(vector<string> v)
+{
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << endl;
+	}
 }
 
 void Ingredient::set_cur_names()
@@ -97,7 +108,7 @@ void Ingredient::add_cur_names(string new_name)
 	{
 		if (cur_names[i] == new_name)
 		{
-			cout << "Ingredient already exists" << endl;
+			cout << "Ingredient already exists in the current ingredient list" << endl;
 			return;
 		}
 	}
@@ -115,7 +126,7 @@ void Ingredient::add_db_names_index(string new_name)
 	{
 		if (db_names[i] == new_name)
 		{
-			cout << "Ingredient already exists" << endl;
+			cout << "Ingredient already exists in the ingredient database" << endl;
 			return;
 		}
 	}
@@ -128,6 +139,73 @@ void Ingredient::add_db_names_index(string new_name)
 	set_db_index();
 }
 
+void Ingredient::del_cur_names(string del_name)
+{
+	vector<string> temp = get_cur_names();
+	
+	int i;
+	for (i = 0; i < temp.size(); i++)
+	{
+		if (temp[i] == del_name)
+		{
+			// delete item and return
+			temp.erase(temp.begin()+1);
+			break;
+		}
+	}
+	
+	if (i > temp.size())
+	{
+		cout << "Ingredient does not exist in the current ingredient list" << endl;
+		return;
+	}
+	
+	ofstream outfile;
+	outfile.open("data.txt", ofstream::trunc);
+	
+	for (int j = 0; j < temp.size(); j++)
+	{
+		outfile << temp[j] << endl;
+	}
+	
+	outfile.close();
+	set_cur_names();
+}
+
+void Ingredient::del_db_names_index(string del_name) // call del_cur_names
+{
+	vector<string> temp = get_db_names();
+	
+	int i;
+	for (i = 0; i < temp.size(); i++)
+	{
+		if (temp[i] == del_name)
+		{
+			// delete item and return
+			temp.erase(temp.begin()+1);
+			break;
+		}
+	}
+	
+	if (i > temp.size())
+	{
+		cout << "Ingredient does not exist in the ingredient database" << endl;
+		return;
+	}
+	
+	ofstream outfile;
+	outfile.open("data2.txt", ofstream::trunc);
+	
+	for (int j = 0; j < temp.size(); j++)
+	{
+		outfile << temp[j] << ":" << j << endl;
+	}
+	
+	outfile.close();
+	set_db_names();
+	set_db_index();
+}
+
 int main()
 {
     Ingredient in = Ingredient();
@@ -135,28 +213,53 @@ int main()
 	in.set_db_names();
 	in.set_db_index();
 
-    vector<string> list = in.get_cur_names();
-    
-	cout << endl << "Testing adding function" << endl;
-	in.add_cur_names("egg");
-	list = in.get_cur_names();
+	bool exit = false;
+	string choice;
+	string ingr;
 	
-	for (int i = 0; i < list.size(); i++)
+	while (exit == false)
 	{
-		cout << list[i] << endl;
-	}
-	
-	vector<string> db_str = in.get_db_names();
-	vector<int> db_int = in.get_db_index();
-	
-	cout << endl << "Testing db add function" << endl;
-	in.add_db_names_index("sugar");
-	db_str = in.get_db_names();
-	db_int = in.get_db_index();
-	
-	for (int i = 0; i < db_str.size(); i++)
-	{
-		cout << db_str[i] << ":" << db_int[i] << endl;
+		cout << "Press 1 to view current available ingredients" << endl;
+		cout << "Press 2 to add a new available ingredient" << endl;
+		cout << "Press 3 to delete an ingredient" << endl;
+		cout << "Press 4 to view ingredient database" << endl;
+		cout << "Press 5 to add entry in ingredient database" << endl;
+		getline(cin, choice);
+		
+		switch (stoi(choice))
+		{
+			case 1:
+			{
+				in.display(in.get_cur_names());
+				break;
+			}
+			case 2:
+			{
+				cout << "Enter the ingredient you want to add to the current ingredient list:" << endl;
+				getline(cin, ingr);
+				in.add_cur_names(ingr);
+				break;
+			}
+			case 3:
+			{
+				cout << "Enter the ingredient you want to delete from the current ingredient list:" << endl;
+				getline(cin, ingr);
+				in.del_cur_names(ingr);
+				break;
+			}
+			case 4:
+			{
+				in.display(in.get_db_names());
+				break;
+			}
+			case 5:
+			{
+				cout << "Enter the ingredient you want to add to the ingredient database:" << endl;
+				getline(cin, ingr);
+				in.add_db_names_index(ingr);
+				break;
+			}
+		}
 	}
 	
 	return 0;

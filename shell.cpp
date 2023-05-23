bc170fc91392cfeb2faeaa2ee9e2d0831997a654
JeffafaV <47927752+JeffafaV@users.noreply.github.com>
2023-05-22 22:43:25 -0700

    Finished all Shell functions
    Finished coding all the member functions for the Shell class.

--- shell.cpp
+++ shell.cpp
@@ -41,6 +41,7 @@ void Shell::deletingCIngr()
 
 void Shell::viewingCIngr()
 {
+	cout << "List of currently available ingredients:" << endl;
 	ci.viewCIngr();
 }
 
@@ -67,7 +68,7 @@ void Shell::addingDish()
 	
 	if (dish.empty() == true || ingrs.empty() == true || desc.empty() == true)
 	{
-		cout << "Input can't be empty" << endl;
+		cout << "Any input can't be empty" << endl;
 		return;
 	}
 	
@@ -76,35 +77,106 @@ void Shell::addingDish()
 
 void Shell::editingDishName()
 {
+	string dish;
+	cout << "Enter dish to edit" << endl;
+	getline(cin, dish);
 	
+	string newName;
+	cout << "Enter new name of dish" << endl;
+	getline(cin, newName);
+	
+	if(dish.empty() == true || newName.empty() == true)
+	{
+		cout << "Any input can't be empty" << endl;
+		return;
+	}
+	
+	rb.edtDishName(dish, newName);
 }
 
 void Shell::editingDishIngr()
 {
+	cout << "Enter dish to edit" << endl;
+	getline(cin, dish);
 	
+	cout << "Enter new ingredients one by one" << endl;
+	cout << "Enter dish ingredient. Enter quit to stop" << endl;
+	getline(cin, ingr);
+	
+	vector<string> ingrs;
+	while (ingr != "quit")
+	{
+		ingrs.push_back(ingr);
+		cout << "Enter dish ingredient. Enter quit to stop" << endl;
+		getline(cin, ingr);
+	}
+	
+	if (dish.empty() == true || ingrs.empty() == true)
+	{
+		cout << "Any input can't be empty" << endl;
+		return;
+	}
+	
+	rb.edtDishIngr(dish, ingrs);
 }
 
 void Shell::editingDishDesc()
 {
+	string dish;
+	cout << "Enter dish to edit" << endl;
+	getline(cin, dish);
 	
+	string desc;
+	cout << "Enter new description" << endl;
+	getline(cin, desc);
+	
+	if (dish.empty() == true || desc.empty() == true)
+	{
+		cout << "Any input can't be empty" << endl;
+		return;
+	}
+	
+	rb.edtDishDesc(dish, desc);
 }
 
 void Shell::deletingDish()
 {
+	string dish;
+	cout << "Enter dish to delete" << endl;
+	getline(cin, dish);
 	
+	if (dish.empty() == true)
+	{
+		cout << "Input can't be empty" << endl;
+		return;
+	}
+	
+	rb.delDish(dish);
 }
 
 void Shell::viewingDishDetails()
 {
+	string dish;
+	cout << "Enter dish to view" << endl;
+	getline(cin, dish);
 	
+	if (dish.empty() == true)
+	{
+		cout << "Input can't be empty" << endl;
+		return;
+	}
+	
+	rb.viewDishDetails(dish);
 }
 
 void Shell::viewingAllDishes()
 {
-	
+	cout << "List of dishes stored in the recipe book:" << endl;
+	rb.viewAllDishes();
 }
 
 void Shell::cookableDishes()
 {
-	
+	cout << "Cookable dishes:" << endl;
+	srch.cookable(ci, rb);
 }
